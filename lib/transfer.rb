1e4bc481ca6e7671caf84d55feafccfa03d465c3
Mitchel Parish <mitchelparish@gmail.com>
2019-01-31 16:38:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,15 @@ class Transfer
   end
 
   def execute_transaction
-  end
+    if @sender.balance > @amount && status =="pending" 
+      @sender.balance -= @amount 
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end 
 
   def reverse_transfer
   end
